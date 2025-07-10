7bc22c2f78271162e33087fbd5bd440c249d6ce3
tommery <tommery@gmail.com>
2025-07-10 14:25:07 +0300

    My First Commit

---
+++ user-rest/src/main/java/com/sita/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.sita.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from User Service!";
+    }
+}
