bbd7595cdec82d9fa85ea281b41e6f48326bddee
tommery <tommery@gmail.com>
2025-07-13 14:59:13 +0300

    take 7

--- user-rest/src/main/java/com/sita/controller/UserController.java
+++ user-rest/src/main/java/com/sita/controller/UserController.java
@@ -13,6 +13,6 @@ public class UserController {
     
     @GetMapping("/git")
     public String git() {
-        return "CI/CD succesfully completed!!!  take 6";
+        return "CI/CD succesfully completed!!!  take 7";
     }
 }
