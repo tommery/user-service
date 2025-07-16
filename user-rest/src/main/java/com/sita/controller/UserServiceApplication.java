4b73394c1728a90b58738dbacb713a60ed066854
tommery <tommery@gmail.com>
2025-07-16 22:16:41 +0300

    scan package

--- user-rest/src/main/java/com/sita/controller/UserServiceApplication.java
+++ user-rest/src/main/java/com/sita/controller/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.sita.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sita")
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
